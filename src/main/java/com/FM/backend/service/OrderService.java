f2a501761547228768707644d87ba87be26fdd76
ALANT <rjsgh7557@naver.com>
2025-06-01 06:59:08 +0900

    order

--- src/main/java/com/FM/backend/service/OrderService.java
+++ src/main/java/com/FM/backend/service/OrderService.java
@@ -1,9 +1,19 @@
 package com.FM.backend.service;
 
+import java.util.List;
+
 import com.FM.backend.model.OrderVO;
 
 public interface OrderService {
-
     void saveOrder(OrderVO order);
 
+    List<OrderVO> getOrdersByMember(String memberMail);
+
+    void updateStatus(int orderId, String status);
+
+    void markAsRefunded(int orderId);
+    
+    OrderVO getOrderById(int orderId);
+
+    public List<OrderVO> getRecentOrders(String memberMail);
 }
